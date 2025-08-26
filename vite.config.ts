3799af95f1862efcb22a6fd49d67745c4e17008c
Sergey Getman <sergeygetman2020@gmail.com>
2025-08-26 14:17:10 +0300

    change dependenciews

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Kanban_DRP/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
