c4c0d62950f327ace2540a53862a2933169972ed
Sergey Getman <sergeygetman2020@gmail.com>
2025-09-08 03:36:18 +0300

    refactor master

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,65 +1,63 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-        project: './tsconfig.json',
-    },
-    plugins: ['react-refresh', '@typescript-eslint'],
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-    rules: {
 
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
 
+    'prettier/prettier': 'warn',
 
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-
-
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off',
+    'no-constant-condition': ['warn', { checkLoops: false }],
+    'no-empty': 'warn',
+  },
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx'],
+      rules: {
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off',
-
-
-        'prettier/prettier': 'warn',
-
-
-        'no-constant-condition': ['warn', { checkLoops: false }],
-        'no-empty': 'warn',
+      },
     },
-    overrides: [
-        {
-            files: ['**/*.js', '**/*.jsx'],
-            rules: {
-                '@typescript-eslint/no-unsafe-assignment': 'off',
-                '@typescript-eslint/no-unsafe-call': 'off',
-                '@typescript-eslint/no-unsafe-member-access': 'off',
-            },
-        },
-    ],
+  ],
 };
