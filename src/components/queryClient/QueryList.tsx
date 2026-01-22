c6ac7724821359ddb2f6367c2413fd74a877f3ba
All1in <bm3023068@gmail.com>
2026-01-22 19:07:47 +0200

    Restore develop state and adjust query list example

--- src/components/queryClient/QueryList.tsx
+++ src/components/queryClient/QueryList.tsx
@@ -1,90 +1,54 @@
-/* eslint-disable prettier/prettier */
 import React, { useCallback, useEffect, useState } from 'react';
-import { QueryTableWrapper } from '@/components/StyledComponent/QueryList.style';
+import {
+  QueryListStyle,
+  QueryListWrapper,
+} from '@/components/StyledComponent/QueryList.style';
 import { useQuery } from '@tanstack/react-query';
+import Todos from './ToDos';
 
-import Users from './Users';
 const URL = 'https://jsonplaceholder.typicode.com/todos';
-const fetchUsers = () => fetch(URL).then(res => res.json);
 
-export type IArrTestConf = {
-  link: string;
-  url: string;
-};
+const fetchUsers = (): Promise<unknown> =>
+  fetch(URL).then(res => res.json);
 
-const arrTestConfig: IArrTestConf[] = [
-  {
-    link: 'ENUM_link',
-    url: 'www.google.com',
-  },
-  {
-    link: 'ENUM_link',
-    url: 'www.facebook.com',
-  },
-  {
-    link: 'ENUM_link',
-    url: 'www.apple.com',
-  },
-];
-
-function QueryListComponent() {
+const MyComponent: React.FC = () => {
   const { data, isLoading } = useQuery({
     queryKey: ['todos'],
     queryFn: fetchUsers,
   });
 
-  console.log('data:', data, typeof data);
-  if (isLoading) return <div>Загрузка...</div>;
-  return <div>{JSON.stringify(data)}</div>;
-}
+  if (isLoading) {
+    return <div>Загрузка...</div>;
+  }
 
-export const GetQueryListEnumLinks = () => {
-  return (
-    <>
-      <div style={{ backgroundColor: 'grey', height: '100%' }}>
-        {arrTestConfig.map((el, idx) => (
-          <ul key={idx}>
-            <li>
-              {el.link}: 
-              {el.url}
-            </li>
-          </ul>
-        ))}
-      </div>
-    </>
-  );
+  return <div>{JSON.stringify(data)}</div>;
 };
 
-const QueryList = () => {
-  const [status, setStatus] = useState(false);
-  const [mobileView, setMobileView] = useState(false);
-  window.addEventListener('resize', () => {
-    if (window.innerWidth < 600) {
-      setMobileView(true);
-    } else {
-      setMobileView(false);
-    }
-  });
+const QueryList: React.FC = () => {
+  const [status, setStatus] = useState<boolean>(false);
 
   const checkedFn = useCallback(() => {
     setStatus(true);
-  }, [status]);
+  }, [setStatus, status]);
 
   useEffect(() => {
-    setTimeout(() => {
+    const timerId = setTimeout(() => {
       checkedFn();
     }, 3000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
   });
+
   return (
-    <>
-      <QueryTableWrapper mobileView={mobileView}>
-        <Users />
-      </QueryTableWrapper>
-      <QueryTableWrapper mobileView={mobileView}>
-        <QueryListComponent />
-        <GetQueryListEnumLinks />
-      </QueryTableWrapper>
-    </>
+    <QueryListStyle>
+      <h1>Hello Query list</h1>
+      {/* <MyComponent /> */}
+      <QueryListWrapper checked={status}>
+        <Todos />
+      </QueryListWrapper>
+    </QueryListStyle>
   );
 };
 
