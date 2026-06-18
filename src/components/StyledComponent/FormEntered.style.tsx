e80d060e18ffd129c51919cfc825301f07b17562
Sergey Getman <SergeyGetman2020@gmail.com>
2026-06-18 23:57:28 +0300

    refactor auth

--- src/components/StyledComponent/FormEntered.style.tsx
+++ src/components/StyledComponent/FormEntered.style.tsx
@@ -35,3 +35,27 @@ export const FormEnteredFormButton = styled(Box)(() => ({
     cursor: 'pointer',
   },
 }));
+
+export const FormEBox = styled(Typography)(() => ({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gridGap: '30px',
+  margin: '12% auto',
+
+  '& input': {
+
+    padding: '5px',
+    fontSize: '15px',
+    fontWeight: '700',
+    fontFamily: 'cursive',
+    borderRadius: '20px',
+    color: 'grey',
+    boxShadow: '-2px 0px 20px 4px #ce47d2a8',
+  },
+  '& input:hover': {
+    backgroundColor: 'rgb(66 66 85 / 37%)',
+    cursor: 'pointer',
+  },
+}));
--- src/components/entered/FormRegisterComponent.tsx
+++ src/components/entered/FormRegisterComponent.tsx
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
+import React, {useActionState, useState} from 'react';
 import {
+  FormEBox,
   FormEnteredForm,
   FormEnteredFormButton,
 } from '@/components/StyledComponent/FormEntered.style';
 import ButtonElement from '@/librariesComponent';
+import { useFormStatus } from 'react-dom';
 
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 import { Box } from '@mui/material';
+import firebase from "firebase/compat";
+import auth = firebase.auth;
+
+function SubmitBtn() {
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={pending}>
+      {pending ? 'Отправка...' : 'Войти'}
+    </button>
+  );
+}
 
 const FormregisterComponent = ({ title, handleClick }) => {
+  const loginAction = async (previousState: any, formData: FormData) => {
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    const emailSendForm = formData.get('email');
+    const passwordSecondForm = formData.get('password');
+    console.log('emailSendForm', emailSendForm);
+    console.log('passwordSecondForm', passwordSecondForm);
+    return { success: true, message: "Данные успешно получены!" };
+  };
+
+  const [state, formAction] = useActionState(loginAction, {
+    susses: false,
+    message: ""
+  })
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -44,6 +71,17 @@ const FormregisterComponent = ({ title, handleClick }) => {
           />
         </FormEnteredFormButton>
       </FormEnteredForm>
+      <FormEBox >
+
+
+      <form action={loginAction}>
+        <input name="email" type="email"  />
+
+        <input name="password" type="password" />
+        <SubmitBtn />
+        {state.susses && <p style={{backgroundColor: 'white', color: 'black'}}>{state.message}</p>}
+      </form>
+      </FormEBox>
     </>
   );
 };
